4b13b163b48b2eb4783c7e1b89c1c7ccc7d31eff
Adam Howell <bigguywhokills@yahoo.com>
2014-07-17 12:29:09 -0600

    2
    Changed the paint color to black in view.java and cleaned up the code,
    slightly.

--- app/src/main/java/com/adamjhowell/androidcompass/view.java
+++ app/src/main/java/com/adamjhowell/androidcompass/view.java
@@ -17,7 +17,7 @@ public class view extends View
 	private float position = 0;
 
 
-	public view(Context context)
+	public view( Context context )
 	{
 		super( context );
 		init();
@@ -31,22 +31,22 @@ public class view extends View
 		paint.setStrokeWidth( 2 );
 		paint.setTextSize( 25 );
 		paint.setStyle( Paint.Style.STROKE );
-		paint.setColor( Color.WHITE );
+		paint.setColor( Color.BLACK );
 	}
 
 
 	@Override
-	protected void onDraw(Canvas canvas)
+	protected void onDraw( Canvas canvas )
 	{
 		int xPoint = getMeasuredWidth() / 2;
 		int yPoint = getMeasuredHeight() / 2;
 
 		float radius = ( float ) ( Math.max( xPoint, yPoint ) * 0.6);
 		canvas.drawCircle( xPoint, yPoint, radius, paint );
-		canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), paint );
+		canvas.drawRect( 0, 0, getMeasuredWidth(), getMeasuredHeight(), paint );
 
 		// 3.143 is a good approximation for the circle
-		canvas.drawLine(xPoint, yPoint,
+		canvas.drawLine( xPoint, yPoint,
 			( float ) ( xPoint + radius * Math.sin( ( double ) ( -position ) / 180 * 3.143 ) ),
 			( float ) ( yPoint - radius * Math.cos( ( double ) ( -position ) / 180 * 3.143 ) ), paint );
 		canvas.drawText( String.valueOf( position ), xPoint, yPoint, paint );
